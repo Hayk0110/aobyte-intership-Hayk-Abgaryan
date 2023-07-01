8b7ee7f9718a160ac718d01cebc5dd23eaea69c6
Hayk Abgaryan <hayk.abgaryan.0110@gmail.com>
2023-07-02 02:33:45 +0400

    added DOM task

---
+++ Homework2/DOM/js/app.js
@@ -0,0 +1,17 @@
+import el from "./el.js";
+
+const tree =
+      el("form", {action: '/some_action'}, [
+        el("label", {for: 'name'}, "First name:"),
+        el("br", {}, null),
+        el("input", {type: 'text', id: 'name', name: 'name', value: "My name"}, null),
+        el("br", {}, null),
+        el("label", {for: 'last_name'}, "Last name:"),
+        el("br", {}, null),
+        el("input", {type: 'text', id: 'last_name', name: 'last_name', value: "My second name"}, null),
+        el("br", {}, null),
+        el("input", {type: 'submit', value: "Submit"}, null),
+      ]);
+    document.getElementById("root").appendChild(tree.draw());
+
+
